289d089f25134429150a6ab09c1b86cc83f512dd
neko113 <alstn113@gmail.com>
2022-04-08 08:25:15 +0900

    [feat] : add mysql and typeorm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import configuration from '@/config/configuration';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -9,6 +10,20 @@ import configuration from '@/config/configuration';
       envFilePath: [process.env.NODE_ENV ? 'dev' : '.env'],
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('mysql.host'),
+        port: +configService.get<number>('mysql.port'),
+        username: configService.get<string>('mysql.username'),
+        password: configService.get<string>('mysql.password'),
+        database: configService.get<string>('mysql.database'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
     }),
   ],
   controllers: [],
