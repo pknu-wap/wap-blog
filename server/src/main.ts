2b41ddce1a59252190860ee6bb616736ba874840
neko113 <alstn113@gmail.com>
2022-04-08 08:27:40 +0900

    [feat] : add swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,12 +2,14 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { Logger } from '@nestjs/common';
+import { setupSwagger } from '@/utils/setupSwagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger();
   const configService = app.get(ConfigService);
   const port = +configService.get<number>('SERVER_PORT');
+  setupSwagger(app);
   await app.listen(port);
   logger.verbose(`Listening On Port ${port}`);
 }
