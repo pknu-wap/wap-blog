7fdaed6a9fc5039220af361317691a2f1ad72893
cornpip <chdnjf1375@naver.com>
2022-04-18 18:40:28 +0900

    CRUD(이미지 제외)

--- server/src/article/dto/update-article.dto.ts
+++ server/src/article/dto/update-article.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateArticleDto } from './create-article.dto';
 
-export class UpdateArticleDto extends PartialType(CreateArticleDto) {}
+export class UpdateArticleDto extends PartialType(CreateArticleDto) {
+}
--- server/src/article/service/article.service.ts
+++ server/src/article/service/article.service.ts
@@ -1,26 +1,45 @@
 import { Injectable } from '@nestjs/common';
-import { CreateArticleDto } from '../dto/create-article.dto';
-import { UpdateArticleDto } from '../dto/update-article.dto';
+import { UpdateArticleDto, CreateArticleDto } from '@/article/dto/index';
+import { ArticleRepository } from '../repository';
 
 @Injectable()
 export class ArticleService {
-  create(createArticleDto: CreateArticleDto) {
+
+  constructor(
+    private readonly articlerepo: ArticleRepository,
+  ) {}
+
+  async allarticle() {
+    // 넘겨줄 순서, 개수 고려할 수 있음
+    const all = await this.articlerepo.find();
+    return all;
+  }
+
+  async article(id: number) {
+    const article = await this.articlerepo.findarticle(id)
+    return article
+  }
+
+  async create(dto: CreateArticleDto) {
+    const article = this.articlerepo.create(dto);
+    await this.articlerepo.save(article);
     return 'This action adds a new article';
   }
 
-  findAll() {
-    return `This action returns all article`;
+  async update(id: number, dto: UpdateArticleDto){
+    await this.articlerepo.uparticle(id, dto);
+    return
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} article`;
+  async remove(id: number) {
+    //updatedat 도 같이 갱신된다.
+    //삭제된 열 지우는 부분 필요
+    await this.articlerepo.softDelete({id: id})
+    return `${id} article을 삭제했습니다.`
   }
-
-  update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} article`;
+  
+  async restore(id: number){
+    await this.articlerepo.restore(id);
+    return
   }
 }
