d5b5a23f8cd8e7ce9e64595b5ac5cbe755ec19cc
neko113 <alstn113@gmail.com>
2022-05-01 04:59:06 +0900

    [feat] : user controller에 getCurrentUser 추가

--- client/src/interfaces/user.interface.ts
+++ client/src/interfaces/user.interface.ts
@@ -1,4 +1,5 @@
 export interface IUser {
+  id: string;
   email: string;
   username: string;
 }
--- server/src/user/service/user.service.ts
+++ server/src/user/service/user.service.ts
@@ -4,4 +4,9 @@ import { UserRepository } from '@/user/repository/user.repository';
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
+
+  async getCurrentUser(userId: string) {
+    const user = await this.userRepository.findById(userId);
+    return { id: user.id, email: user.email, username: user.username };
+  }
 }
