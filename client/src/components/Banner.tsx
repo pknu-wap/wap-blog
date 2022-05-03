b7bff0c22375e95fe2c8c498a50a68a8a3c298d6
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-05-03 20:03:01 +0900

    Banner 컴포넌트 생성

---
+++ client/src/components/Banner.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import tw from 'tailwind-styled-components';
+
+const BannerColor = styled.span`
+  background-color: ${props => props.theme.bannerColor};
+`;
+
+const BannerComponent = tw(BannerColor)`
+flex
+justify-center
+items-center
+w-full
+py-16
+bg-yellow-300
+text-white
+text-4xl
+shadow-inner
+mb-8
+`;
+
+const Banner = () => {
+  return (
+    <>
+      <BannerComponent>BANNER</BannerComponent>
+    </>
+  );
+};
+
+export default Banner;
