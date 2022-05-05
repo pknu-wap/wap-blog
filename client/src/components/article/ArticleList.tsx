9c740f812cac05fe0e91b7ede7c2a7377429151e
neko113 <alstn113@gmail.com>
2022-05-05 22:22:19 +0900

    [fix] : pull 대신 적다히 비교하면서 client꺼 합침

---
+++ client/src/components/article/ArticleList.tsx
@@ -0,0 +1,57 @@
+import { useQuery } from 'react-query';
+import styled from 'styled-components';
+import ArticleAPI from '../../api/article';
+import tw from 'tailwind-styled-components';
+import { Link } from 'react-router-dom';
+
+const ArticleContainer = tw.div`
+w-full 
+`;
+
+const Articles = tw.ul`
+w-1/2
+mx-auto
+divide-y-2
+divide-solid
+`;
+const Article = styled.li`
+  padding: 20px 0;
+`;
+
+const ArticleTitleColor = styled.h2`
+  color: ${props => props.theme.textColor};
+`;
+const ArticleTitle = tw(ArticleTitleColor)`
+text-xl
+font-medium
+`;
+const ArticleAuthor = tw.div``;
+const ArticleDescriptonColor = styled.span`
+  color: ${props => props.theme.lightTextColor};
+`;
+const ArticleDescripton = tw(ArticleDescriptonColor)`
+text-lg
+font-light
+`;
+
+const ArticleList = () => {
+  const { data: articleListData } = useQuery('articleList', ArticleAPI.getAll);
+  return (
+    <>
+      <ArticleContainer>
+        <Articles>
+          {articleListData?.map(article => (
+            <Article key={article.id}>
+              <Link key={article.id} to={article.id + ''}>
+                <ArticleAuthor>{article.writer}</ArticleAuthor>
+                <ArticleTitle>{article.title}</ArticleTitle>
+                <ArticleDescripton>{article.description}</ArticleDescripton>
+              </Link>
+            </Article>
+          ))}
+        </Articles>
+      </ArticleContainer>
+    </>
+  );
+};
+export default ArticleList;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,13 @@
 import { Suspense } from 'react';
-import Articles from '../components/Articles';
+import ArticleList from '../components/article/ArticleList';
+import Banner from '../components/Banner';
 import LoadingSpinner from '../components/common/LoadingSpinner';
 
 const Home = () => {
   return (
     <>
       <Suspense fallback={<LoadingSpinner />}>
-        <Articles />
+        <Banner />
       </Suspense>
     </>
   );
