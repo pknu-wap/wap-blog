bbc2d29c0e08c6b43c798a3e27dcf9ec8b8c1b24
neko113 <alstn113@gmail.com>
2022-05-09 19:51:54 +0900

    [feat] : login과 register 페이지 error handling

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,10 +1,14 @@
 import { properties } from '../config/properties';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import tw from 'tailwind-styled-components';
 import AuthAPI from '../api/auth';
 import { useStore } from '../store/store';
+import { useMutation } from 'react-query';
+import { useState } from 'react';
 
 const LoginForm = tw.form`
 border-2
@@ -27,14 +31,46 @@ interface IFormInputs {
   password: string;
 }
 
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email('이메일 형식이어야합니다')
+    .required('필수항목입니다'),
+  password: yup.string().required('필수 항목입니다'),
+});
+
 const LoginPage = () => {
-  const navigate = useNavigate();
-  const { register, handleSubmit } = useForm<IFormInputs>();
   const { setUser } = useStore();
-  const onLogin = async (input: IFormInputs) => {
-    const user = await AuthAPI.signin(input);
-    setUser(user);
-    navigate('/');
+  const [serverError, setServerError] = useState('');
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInputs>({
+    resolver: yupResolver(schema),
+    mode: 'onChange',
+  });
+
+  const mutation = useMutation(
+    'signin',
+    async (body: IFormInputs) => {
+      const user = await AuthAPI.signin(body);
+      return user;
+    },
+    {
+      onSuccess: async data => {
+        setUser(data);
+        navigate('/');
+      },
+      onError: async (e: any) => {
+        setServerError(e.response.data.message);
+      },
+    },
+  );
+
+  const onSubmit = async (data: IFormInputs) => {
+    mutation.mutate(data);
   };
 
   const handleGithubLogin = async () => {
@@ -46,21 +82,24 @@ const LoginPage = () => {
 
   return (
     <>
-      <LoginForm onSubmit={handleSubmit(onLogin)}>
+      <LoginForm onSubmit={handleSubmit(onSubmit)}>
         <LoginInput {...register('email')} type="email" placeholder="이메일" />
+        <p>{errors.email?.message}</p>
         <LoginInput
           {...register('password')}
           type="password"
           placeholder="비밀번호"
         />
+        <p>{errors.password?.message}</p>
+        <p>{serverError}</p>
         <LoginBtn>로그인</LoginBtn>
-        <div>
-          <button onClick={handleGithubLogin}>깃허브</button>
-        </div>
-        <div>
-          <button onClick={handleGoogleLogin}>구글</button>
-        </div>
       </LoginForm>
+      <div>
+        <button onClick={handleGithubLogin}>깃허브</button>
+      </div>
+      <div>
+        <button onClick={handleGoogleLogin}>구글</button>
+      </div>
     </>
   );
 };
