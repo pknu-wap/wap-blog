a9b2347237989d0e89feefb347da3d217c0ea375
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-05-22 22:29:59 +0900

    WritePage image input 추가

--- client/src/pages/WritePage.tsx
+++ client/src/pages/WritePage.tsx
@@ -42,6 +42,29 @@ const WriteDescription = tw(WriteInput)``;
 const WriteBody = styled.textarea`
   color: black;
 `;
+
+const WriteImageColor = styled.label`
+  :hover {
+    color: ${props => props.theme.accentColor};
+    border-color: ${props => props.theme.accentColor};
+  }
+`;
+
+const WriteImage = tw(WriteImageColor)`
+flex 
+h-48 
+w-full 
+cursor-pointer 
+items-center 
+justify-center 
+rounded-md 
+border-2 
+border-dashed 
+border-gray-300 
+text-gray-600
+mb-4
+`;
+
 const WriteBtn = styled.button`
   border-radius: 0.25rem;
   border: 1px solid black;
@@ -147,6 +170,27 @@ const WritePage = () => {
           />
           <p>{errors.description?.message}</p>
         </fieldset>
+        <WriteImage>
+          <svg
+            className="h-12 w-12"
+            stroke="currentColor"
+            fill="none"
+            viewBox="0 0 48 48"
+            aria-hidden="true"
+          >
+            <path
+              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <input
+            type="file"
+            className="hidden"
+            accept="image/png, image/jpeg, image/jpg"
+          />
+        </WriteImage>
         <fieldset>
           <WriteBody {...register('body')} rows={8} placeholder="내용" />
           <p>{errors.body?.message}</p>
