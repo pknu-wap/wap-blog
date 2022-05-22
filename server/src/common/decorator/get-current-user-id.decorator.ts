887f853534189626e5ccbd3c0f53ed8b201ac4e1
neko113 <alstn113@gmail.com>
2022-05-22 18:15:14 +0900

    feat : express Request에서 userId를  global declare함

--- server/src/common/decorator/get-current-user-id.decorator.ts
+++ server/src/common/decorator/get-current-user-id.decorator.ts
@@ -4,6 +4,6 @@ export const GetCurrentUserId = createParamDecorator(
   (_data: unknown, context: ExecutionContext): number => {
     // middleware를거치고 guard를 거치고 req.body에 있으면 그거 반환
     const req = context.switchToHttp().getRequest();
-    return req.body.userId;
+    return req.userId;
   },
 );
